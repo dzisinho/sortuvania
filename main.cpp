eaaf331c92b29129e248cbd98e2458ad7b3373d5
dzisinho <dzisbogdan26@gmail.com>
2025-01-27 16:35:42 +0200

    completed

--- main.cpp
+++ main.cpp
@@ -1,1 +1,38 @@
  
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        bool swapped = false;
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) break; // Якщо перестановок немає, сортування завершено
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = { 5, 2, 9, 1, 5, 6 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Before sorting: ";
+    printArray(arr, size);
+
+    bubbleSort(arr, size);
+
+    cout << "After sorting: ";
+    printArray(arr, size);
+
+    return 0;
+}
